52854eee4cb1f83864d33c7a76a2eeaed664282c
theBlackMAze <talktodonnybronson@gmail.com>
2017-05-31 10:57:02 +0100

    checking if its the environment

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,13 @@
+var postsData = [
+  {
+    title: 'Bowie hip sync',
+    url: 'http://s-j.io/bowie'
+  },
+  {
+    title: 'pocket jockey',
+    url: 'http://s-j.io/pj'
+  }
+];
+Template.postsList.helpers({
+  posts: postsData
+});
